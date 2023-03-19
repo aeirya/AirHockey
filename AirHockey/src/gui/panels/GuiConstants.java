fd09527bc9aa693ddf566812264eb28a31f9e217
aeirya <aeirya@gmail.com>
2023-03-19 14:17:48 +0330

    add gitignore for ide files

---
+++ AirHockey/src/gui/panels/GuiConstants.java
@@ -0,0 +1,13 @@
+package gui.panels;
+
+/**
+ * This class includes the constants used by the gui package
+ */
+public class GuiConfig {
+    private GuiConfig() {
+
+    }
+
+    public static final int WINDOW_WIDTH;
+    public static final int WINDOW_HEIGHT;
+}
---
+++ AirHockey/src/gui/panels/Window.java
@@ -0,0 +1,2 @@
+package gui.panels;public class Window {
+}
